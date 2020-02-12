243fa09df8eab64d77ff9c2f424e7df425f9e800
Kemal Akkoyun <kakkoyun@gmail.com>
2020-02-12 18:12:47 +0100

    Fix lint

--- test/load.sh
+++ test/load.sh
@@ -134,7 +134,7 @@ number_of_concurrent_queries="10"
 while getopts "h?o:r:c:m:q:" opt; do
     case "$opt" in
     h)
-        printf "$usage\n"
+        echo "$usage"
         exit 0
         ;;
     o)
