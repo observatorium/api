3c53e88fe4b5ea1cab0e0f8f5e57119ae400dc2c
Kemal Akkoyun <kakkoyun@users.noreply.github.com>
2020-04-06 10:09:49 +0200

    Redirect requests to root, to UI (#10)
    * Redirect requests to root, to UI
    
    * Remove local test file added by mistake
    
    * Reflect schema changes
    
    * Use path.Join

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"net/http"
+	"path"
 	"time"
 
 	"github.com/observatorium/observatorium/internal/proxy"
@@ -39,46 +40,67 @@ func New(logger log.Logger, reg *prometheus.Registry, opts ...Option) Server {
 		o.apply(&options)
 	}
 
-	ins := newInstrumentationMiddleware(reg)
-	p := prober.New(logger)
-
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Recoverer)
+	r.Use(middleware.StripSlashes)
 	r.Use(middleware.Timeout(options.timeout))
 
 	if options.profile {
 		r.Mount("/debug", middleware.Profiler())
 	}
 
+	ins := newInstrumentationMiddleware(reg)
+	p := prober.New(logger)
+
 	registerProber(r, p)
+
+	uiPath := "/ui/metrics/v1"
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, path.Join(uiPath, "graph"), http.StatusMovedPermanently)
+	})
 
 	r.Get("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		promhttp.InstrumentMetricHandler(reg, promhttp.HandlerFor(reg, promhttp.HandlerOpts{})).ServeHTTP(w, r)
 	})
 
 	if options.metricsUIEndpoint != nil {
-		r.Get("/ui/metrics/v1/*",
-			ins.newHandler("ui", proxy.New(logger, "/ui/metrics/v1", options.metricsUIEndpoint, options.proxyOptions...)))
+		r.Get(path.Join(uiPath, "*"),
+			ins.newHandler("ui", proxy.New(logger, uiPath, options.metricsUIEndpoint, options.proxyOptions...)))
 	}
 
 	namespace := "/api/metrics/v1"
 	r.Route(namespace, func(r chi.Router) {
 		if options.metricsReadEndpoint != nil {
 			r.Get("/api/v1/query",
-				ins.newHandler("query", proxy.New(logger, namespace+"/api/v1", options.metricsReadEndpoint, options.proxyOptions...)))
+				ins.newHandler("query", proxy.New(logger, path.Join(namespace, "api/v1"), options.metricsReadEndpoint, options.proxyOptions...)))
 
 			r.Get("/api/v1/query_range",
-				ins.newHandler("query_range", proxy.New(logger, namespace+"/api/v1", options.metricsReadEndpoint, options.proxyOptions...)))
+				ins.newHandler("query_range", proxy.New(logger, path.Join(namespace, "api/v1"), options.metricsReadEndpoint, options.proxyOptions...)))
 
 			r.Get("/api/v1/*",
-				ins.newHandler("read", proxy.New(logger, namespace+"/api/v1", options.metricsReadEndpoint, options.proxyOptions...)))
+				ins.newHandler("read", proxy.New(logger, path.Join(namespace, "api/v1"), options.metricsReadEndpoint, options.proxyOptions...)))
 		}
 
 		writePath := "/write"
 		r.Post(writePath,
-			ins.newHandler("write", proxy.New(logger, namespace+writePath, options.metricsWriteEndpoint, options.proxyOptions...)))
+			ins.newHandler("write", proxy.New(logger, path.Join(namespace, writePath), options.metricsWriteEndpoint, options.proxyOptions...)))
+	})
+
+	// NOTICE: Following redirects added to be compatible with existing Read UI.
+	// Paths are explicitly specified to prevent unnecessary request to read handler.
+	r.Get("/graph", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/ui/v1/metrics/graph", http.StatusMovedPermanently)
+	})
+
+	r.Get("/stores", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/ui/v1/metrics/stores", http.StatusMovedPermanently)
+	})
+
+	r.Get("/status", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/ui/v1/metrics/status", http.StatusMovedPermanently)
 	})
 
 	p.Healthy()
