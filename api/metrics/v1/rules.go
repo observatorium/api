efc673c86306f2e5f93dc9880cd5de0644987fa4
Saswata Mukherjee <saswataminsta@yahoo.com>
2022-01-27 17:38:45 +0530

    Enforce tenant labels on rule storage PUT request (#219)
    * Enforce tenant labels on rule write
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>
    
    * Implement suggestions
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>
    
    * Remove marshalRules
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>
    
    * Update tests
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>
    
    * Enforce tenant labels on rule read
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>
    
    * Add enforceLabelsInRules
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- api/metrics/v1/rules.go
+++ api/metrics/v1/rules.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"bytes"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -11,6 +12,43 @@ import (
 	"github.com/observatorium/api/authentication"
 	"github.com/observatorium/api/rules"
 )
+
+func enforceLabelsInRules(rawRules rules.Rules, tenantLabel string, tenantID string) {
+	for i := range rawRules.Groups {
+		for j := range rawRules.Groups[i].Rules {
+			switch r := rawRules.Groups[i].Rules[j].(type) {
+			case rules.RecordingRule:
+				if r.Labels.AdditionalProperties == nil {
+					r.Labels.AdditionalProperties = make(map[string]string)
+				}
+
+				r.Labels.AdditionalProperties[tenantLabel] = tenantID
+				rawRules.Groups[i].Rules[j] = r
+			case rules.AlertingRule:
+				if r.Labels.AdditionalProperties == nil {
+					r.Labels.AdditionalProperties = make(map[string]string)
+				}
+
+				r.Labels.AdditionalProperties[tenantLabel] = tenantID
+				rawRules.Groups[i].Rules[j] = r
+			}
+		}
+	}
+}
+
+func unmarshalRules(r io.Reader) (rules.Rules, error) {
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		return rules.Rules{}, err
+	}
+
+	var rawRules rules.Rules
+	if err := yaml.Unmarshal(body, &rawRules); err != nil {
+		return rules.Rules{}, err
+	}
+
+	return rawRules, nil
+}
 
 type rulesHandler struct {
 	client      rules.ClientInterface
@@ -52,45 +90,20 @@ func (rh *rulesHandler) get(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	rawRules, err := unmarshalRules(resp.Body)
 	if err != nil {
-		http.Error(w, "error listing rules", http.StatusInternalServerError)
-		return
-	}
-
-	var rawRules rules.Rules
-	if err := yaml.Unmarshal(body, &rawRules); err != nil {
 		level.Error(rh.logger).Log("msg", "could not unmarshal rules", "err", err.Error())
 		http.Error(w, "error unmarshaling rules", http.StatusInternalServerError)
 
 		return
 	}
 
-	for i := range rawRules.Groups {
-		for j := range rawRules.Groups[i].Rules {
-			switch r := rawRules.Groups[i].Rules[j].(type) {
-			case rules.RecordingRule:
-				if r.Labels.AdditionalProperties == nil {
-					r.Labels.AdditionalProperties = make(map[string]string)
-				}
+	enforceLabelsInRules(rawRules, rh.tenantLabel, id)
 
-				r.Labels.AdditionalProperties[rh.tenantLabel] = id
-				rawRules.Groups[i].Rules[j] = r
-			case rules.AlertingRule:
-				if r.Labels.AdditionalProperties == nil {
-					r.Labels.AdditionalProperties = make(map[string]string)
-				}
-
-				r.Labels.AdditionalProperties[rh.tenantLabel] = id
-				rawRules.Groups[i].Rules[j] = r
-			}
-		}
-	}
-
-	body, err = yaml.Marshal(rawRules)
+	body, err := yaml.Marshal(rawRules)
 	if err != nil {
-		level.Error(rh.logger).Log("msg", "could not marshal YAML", "err", err.Error())
-		http.Error(w, "error marshaling YAML", http.StatusInternalServerError)
+		level.Error(rh.logger).Log("msg", "could not marshal rules YAML", "err", err.Error())
+		http.Error(w, "error marshaling rules YAML", http.StatusInternalServerError)
 
 		return
 	}
@@ -107,7 +120,31 @@ func (rh *rulesHandler) put(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "error finding tenant", http.StatusUnauthorized)
 	}
 
-	resp, err := rh.client.SetRulesWithBody(r.Context(), tenant, r.Header.Get("Content-type"), r.Body)
+	id, ok := authentication.GetTenantID(r.Context())
+	if !ok {
+		http.Error(w, "error finding tenant ID", http.StatusUnauthorized)
+		return
+	}
+
+	rawRules, err := unmarshalRules(r.Body)
+	if err != nil {
+		level.Error(rh.logger).Log("msg", "could not unmarshal rules", "err", err.Error())
+		http.Error(w, "error unmarshaling rules", http.StatusInternalServerError)
+
+		return
+	}
+
+	enforceLabelsInRules(rawRules, rh.tenantLabel, id)
+
+	body, err := yaml.Marshal(rawRules)
+	if err != nil {
+		level.Error(rh.logger).Log("msg", "could not marshal rules YAML", "err", err.Error())
+		http.Error(w, "error marshaling rules YAML", http.StatusInternalServerError)
+
+		return
+	}
+
+	resp, err := rh.client.SetRulesWithBody(r.Context(), tenant, r.Header.Get("Content-type"), bytes.NewReader(body))
 	if err != nil {
 		sc := http.StatusInternalServerError
 		if resp != nil {
