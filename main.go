3e4d83df663e3a945919b1d120067d70d2111644
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2022-05-19 18:39:12 +0200

    Stop OIDC flow when remote writing with bad auth header
    When handling remote write requests, if the Authorization
    header isn't present, we don't want to redirect users back
    to the identity provider for login.
    
    The reason behind this is that tenants can overload the
    identity provider in case they have bad configuration.
    We want to be friendly and help prevent this issue from
    happening.
    
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- main.go
+++ main.go
@@ -520,6 +520,7 @@ func main() {
 
 			r.Use(authentication.WithTenant)
 			r.Use(authentication.WithTenantID(tenantIDs))
+			r.Use(authentication.EnforceAccessTokenPresent("/api/metrics/v1"))
 			r.Use(authentication.WithAccessToken())
 			r.MethodNotAllowed(blockNonDefinedMethods())
 
