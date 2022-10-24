eeee5dfe736b4f40a53fef4fab075433ccf3549b
Gerard Vanloo <gvanloo@redhat.com>
2022-10-24 03:43:17 -0400

    Remove component-base for manual implementation (#396)

--- tls/config.go
+++ tls/config.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
-	"k8s.io/component-base/cli/flag"
 )
 
 // NewClientConfig returns a tls config for the reverse proxy handling if an upstream CA is given.
@@ -43,21 +42,21 @@ func NewServerConfig(logger log.Logger, certFile, keyFile, minVersion, maxVersio
 		return nil, fmt.Errorf("server credentials: %w", err)
 	}
 
-	tlsMinVersion, err := flag.TLSVersion(minVersion)
+	tlsMinVersion, err := parseTLSVersion(minVersion)
 	if err != nil {
-		return nil, fmt.Errorf("TLS version invalid: %w", err)
+		return nil, fmt.Errorf("cannot parse TLS Version: %w", err)
 	}
 
-	tlsMaxVersion, err := flag.TLSVersion(maxVersion)
+	tlsMaxVersion, err := parseTLSVersion(maxVersion)
 	if err != nil {
-		return nil, fmt.Errorf("TLS version invalid: %w", err)
+		return nil, fmt.Errorf("cannot parse TLS Version: %w", err)
 	}
 
 	if tlsMinVersion > tlsMaxVersion {
 		return nil, fmt.Errorf("TLS minimum version can not be greater than maximum version: %v > %v", tlsMinVersion, tlsMaxVersion)
 	}
 
-	cipherSuiteIDs, err := flag.TLSCipherSuites(cipherSuites)
+	cipherSuiteIDs, err := mapCipherNamesToIDs(cipherSuites)
 	if err != nil {
 		return nil, fmt.Errorf("TLS cipher suite name to ID conversion: %v", err)
 	}
@@ -79,6 +78,49 @@ func NewServerConfig(logger log.Logger, certFile, keyFile, minVersion, maxVersio
 	}
 
 	return tlsCfg, nil
+}
+
+func tlsCipherSuites() map[string]uint16 {
+	cipherSuites := map[string]uint16{}
+
+	for _, suite := range tls.CipherSuites() {
+		cipherSuites[suite.Name] = suite.ID
+	}
+	for _, suite := range tls.InsecureCipherSuites() {
+		cipherSuites[suite.Name] = suite.ID
+	}
+
+	return cipherSuites
+}
+
+func parseTLSVersion(rawTLSVersion string) (uint16, error) {
+	switch rawTLSVersion {
+	case "VersionTLS10": 
+		return tls.VersionTLS10, nil
+	case "VersionTLS11": 
+		return tls.VersionTLS11, nil
+	case "VersionTLS12": 
+		return tls.VersionTLS12, nil
+	case "VersionTLS13": 
+		return tls.VersionTLS13, nil
+	default:
+		return 0, fmt.Errorf("unknown TLSVersion: %s", rawTLSVersion)
+	}
+}
+
+func mapCipherNamesToIDs(rawTLSCipherSuites []string) ([]uint16, error) {
+	cipherSuites := []uint16{}
+	allCipherSuites := tlsCipherSuites()
+
+	for _, name := range rawTLSCipherSuites {
+		id, ok := allCipherSuites[name]
+		if !ok {
+			return nil, fmt.Errorf("unknown TLSCipherSuite: %s", name)
+		}
+		cipherSuites = append(cipherSuites, id)
+	}
+
+	return cipherSuites, nil
 }
 
 func parseClientAuthType(rawAuthType string) (tls.ClientAuthType, error) {
