ece7d511c249a4ba4c22526a772e6ee2f1d1dbf3
Robert Jacob <rojacob@redhat.com>
2023-05-31 18:14:20 +0200

    Accept simple namespace regexp

--- authentication/query.go
+++ authentication/query.go
@@ -4,10 +4,15 @@ import (
 	"errors"
 	logqlv2 "github.com/observatorium/api/logql/v2"
 	"github.com/prometheus/prometheus/model/labels"
+	"strings"
 )
 
 const (
 	namespaceMatcherKey = "kubernetes_namespace_name"
+)
+
+var (
+	errWildcardRegexp = errors.New("regular expression with wildcards found")
 )
 
 func parseQueryNamespaces(query string) ([]string, error) {
@@ -17,24 +22,43 @@ func parseQueryNamespaces(query string) ([]string, error) {
 	}
 
 	namespaces := []string{}
+	failWildcard := false
 	expr.Walk(func(expr interface{}) {
 		switch le := expr.(type) {
 		case *logqlv2.StreamMatcherExpr:
 			for _, m := range le.Matchers() {
-				if m.Type != labels.MatchEqual {
-					continue
-				}
-
 				if m.Name != namespaceMatcherKey {
 					continue
 				}
 
-				namespaces = append(namespaces, m.Value)
+				switch m.Type {
+				case labels.MatchEqual:
+					namespaces = append(namespaces, m.Value)
+				case labels.MatchRegexp:
+					values := strings.Split(m.Value, "|")
+					for _, v := range values {
+						if strings.ContainsAny(v, ".+*") {
+							failWildcard = true
+							continue
+						}
+
+						namespaces = append(namespaces, v)
+					}
+				}
+
+				if m.Type != labels.MatchEqual {
+					continue
+				}
+
 			}
 		default:
 			// Do nothing
 		}
 	})
+
+	if failWildcard {
+		return nil, errWildcardRegexp
+	}
 
 	if len(namespaces) == 0 {
 		return nil, errors.New("no namespaces found")
---
+++ authentication/query_test.go
@@ -0,0 +1,47 @@
+package authentication
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parseQueryNamespaces(t *testing.T) {
+	tests := []struct {
+		query   string
+		want    []string
+		wantErr bool
+	}{
+		{
+			query:   `{kubernetes_namespace_name="test"}`,
+			want:    []string{"test"},
+			wantErr: false,
+		},
+		{
+			query:   `{kubernetes_namespace_name="test",kubernetes_namespace_name="test2"}`,
+			want:    []string{"test", "test2"},
+			wantErr: false,
+		},
+		{
+			query:   `{kubernetes_namespace_name=~"test|test2"}`,
+			want:    []string{"test", "test2"},
+			wantErr: false,
+		},
+		{
+			query:   `{kubernetes_namespace_name=~"test|test2|test3.+"}`,
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.query, func(t *testing.T) {
+			got, err := parseQueryNamespaces(tt.query)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseQueryNamespaces() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseQueryNamespaces() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
