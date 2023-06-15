1634c83924cb3c6c28354cbc35ab48c3fbd12a76
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2023-06-15 11:32:09 +0200

    Try different approach for e2e test
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- test/e2e/metrics_test.go
+++ test/e2e/metrics_test.go
@@ -73,7 +73,7 @@ func TestMetricsReadAndWrite(t *testing.T) {
 
 		// Check that API metrics are correct.
 		testutil.Ok(t, api.WaitSumMetricsWithOptions(
-			e2emon.Between(totalQueries+totalWrites-5, totalQueries+totalWrites+5),
+			e2emon.GreaterOrEqual(minimumExpectedQueries+minimumExpectedWrites),
 			[]string{"http_requests_total"},
 		))
 
