5874765c687a797cd5ce4f935591f0242d0dab46
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2023-12-15 20:08:20 +0100

    Add a Redis rate limiter

---
+++ ratelimit/redis.go
@@ -0,0 +1,82 @@
+package ratelimit
+
+import (
+	"context"
+	_ "embed"
+	"strconv"
+	"time"
+
+	"github.com/redis/rueidis"
+)
+
+//go:embed gcra_rate_limit.lua
+var gcraRateLimitScript string
+
+// RedisRateLimiter is a type that represents a rate limiter that uses Redis as its backend.
+// The rate limiting is a leaky bucket implementation using the generic cell rate algorithm.
+// See https://en.wikipedia.org/wiki/Generic_cell_rate_algorithm for details on how this algorithm works.
+type RedisRateLimiter struct {
+	client rueidis.Client
+}
+
+// Ensure RedisRateLimiter implements the SharedRateLimiter interface.
+var _ SharedRateLimiter = (*RedisRateLimiter)(nil)
+
+// NewRedisRateLimiter creates a new instance of RedisRateLimiter.
+func NewRedisRateLimiter(addresses []string) (*RedisRateLimiter, error) {
+	client, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: addresses})
+	if err != nil {
+		return nil, err
+	}
+	return &RedisRateLimiter{client: client}, nil
+}
+
+// GetRateLimits retrieves the rate limits for a given request using a Redis Rate Limiter.
+// It returns the amount of remaining requests, the reset time in milliseconds, and any error that occurred.
+func (r *RedisRateLimiter) GetRateLimits(ctx context.Context, req *request) (remaining, resetTime int64, err error) {
+	inspectScript := rueidis.NewLuaScript(gcraRateLimitScript)
+	rateLimitParameters := []string{
+		strconv.FormatInt(time.Now().UnixMilli(), 10), // now
+		strconv.FormatInt(req.limit, 10),              // burst
+		strconv.FormatInt(req.limit, 10),              // limit
+		strconv.FormatInt(req.duration, 10),           // period
+		"1",                                           // cost
+	}
+	result := inspectScript.Exec(ctx, r.client, []string{req.key}, rateLimitParameters)
+	limited, remaining, resetIn, err := r.parseRateLimitResult(&result)
+	if err != nil {
+		return 0, 0, err
+	}
+	resetTime = time.Now().Add(time.Duration(resetIn)).UnixMilli()
+	if limited {
+		return remaining, resetTime, errOverLimit
+	}
+	return remaining, resetTime, nil
+}
+
+// parseRateLimitResult parses the result of a rate limit check from Redis.
+// It takes a RedisResult as input and returns the parsed rate limit values: whether the request is limited,
+// the number of remaining requests, the reset time in milliseconds, and any error that occurred during parsing.
+func (r *RedisRateLimiter) parseRateLimitResult(result *rueidis.RedisResult) (limited bool, remaining, resetIn int64, err error) {
+	values, err := result.ToArray()
+	if err != nil {
+		return false, 0, 0, err
+	}
+
+	limited, err = values[0].AsBool()
+	if err != nil {
+		return false, 0, 0, err
+	}
+
+	remaining, err = values[1].AsInt64()
+	if err != nil {
+		return false, 0, 0, err
+	}
+
+	resetIn, err = values[3].AsInt64()
+	if err != nil {
+		return false, 0, 0, err
+	}
+
+	return limited, remaining, resetIn, nil
+}
---
+++ ratelimit/redis_test.go
@@ -0,0 +1,155 @@
+package ratelimit
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/efficientgo/core/backoff"
+	"github.com/efficientgo/core/testutil"
+	"github.com/efficientgo/e2e"
+)
+
+func TestRedisRateLimiter_GetRateLimits(t *testing.T) {
+	t.Parallel()
+	// Start isolated environment with given ref.
+	e, err := e2e.New(e2e.WithName("redis-rate-li"))
+	testutil.Ok(t, err)
+	t.Cleanup(e.Close)
+
+	redis := createRedisContainer(e)
+	t.Cleanup(func() { redis.Stop() })
+	e2e.StartAndWaitReady(redis)
+
+	type args struct {
+		ctx context.Context
+		req *request
+	}
+	tests := []struct {
+		name          string
+		args          args
+		totalHits     int64
+		wantRemaining int64
+		// wantResetTimeFunc is used to calculate the expected reset time just before the hits are sent to the rate limiter.
+		wantResetTimeFunc func() time.Time
+		wantErr           error
+		// waitBeforeLastHit is used to wait the given amount of time and then make a last hit on the rate limiter.
+		waitBeforeLastHit time.Duration
+	}{
+		{
+			name: "Single hit, far from limit",
+			args: args{
+				ctx: context.Background(),
+				req: &request{
+					key:      "single-hit",
+					limit:    10,
+					duration: (10 * time.Second).Milliseconds(),
+				},
+			},
+			totalHits:     1,
+			wantRemaining: 9,
+			wantResetTimeFunc: func() time.Time {
+				return time.Now().Add(10 * time.Second)
+			},
+		},
+		{
+			name: "At the edge of the limit",
+			args: args{
+				ctx: context.Background(),
+				req: &request{
+					key:      "edge-hit",
+					limit:    10,
+					duration: (10 * time.Second).Milliseconds(),
+				},
+			},
+			totalHits:     10,
+			wantRemaining: 0,
+			wantResetTimeFunc: func() time.Time {
+				return time.Now().Add(10 * time.Second)
+			},
+		},
+		{
+			name: "Beyond the limit",
+			args: args{
+				ctx: context.Background(),
+				req: &request{
+					key:      "beyond-limit",
+					limit:    10,
+					duration: (10 * time.Second).Milliseconds(),
+				},
+			},
+			totalHits:     11,
+			wantRemaining: 0,
+			wantErr:       errOverLimit,
+			wantResetTimeFunc: func() time.Time {
+				return time.Now().Add(10 * time.Second)
+			},
+		},
+		{
+			name: "Wait for 1 leak",
+			args: args{
+				ctx: context.Background(),
+				req: &request{
+					key:      "wait-for-leak",
+					limit:    10,
+					duration: (10 * time.Second).Milliseconds(),
+				},
+			},
+			totalHits: 2,
+			// Waits for 2 seconds instead of 1 because of rounding in the algorithm.
+			waitBeforeLastHit: 2 * time.Second,
+			wantRemaining:     9,
+			wantResetTimeFunc: func() time.Time {
+				return time.Now().Add(10 * time.Second)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt // Can be removed when Go version >= 1.22 is set in the go.mod file.
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			b := backoff.New(context.Background(), backoff.Config{
+				Min:        100 * time.Millisecond,
+				Max:        1 * time.Second,
+				MaxRetries: 5,
+			})
+
+			var (
+				err error
+				r   *RedisRateLimiter
+			)
+			for b.Reset(); b.Ongoing(); b.Wait() {
+				r, err = NewRedisRateLimiter([]string{redis.Endpoint("http")})
+			}
+			testutil.Ok(t, err)
+			testutil.Assert(t, r != nil)
+
+			var gotRemaining, gotResetTime int64
+			wantResetTime := tt.wantResetTimeFunc()
+			for i := int64(0); i < tt.totalHits; i++ {
+				gotRemaining, gotResetTime, err = r.GetRateLimits(tt.args.ctx, tt.args.req)
+			}
+			if tt.waitBeforeLastHit > 0 {
+				time.Sleep(tt.waitBeforeLastHit)
+				gotRemaining, gotResetTime, err = r.GetRateLimits(tt.args.ctx, tt.args.req)
+			}
+
+			testutil.Equals(t, tt.wantErr, err)
+			testutil.Equals(t, tt.wantRemaining, gotRemaining)
+
+			parsedGotResetTime := time.UnixMilli(gotResetTime)
+			timeDifference := parsedGotResetTime.Sub(wantResetTime).Seconds()
+			testutil.Assert(t, timeDifference <= 1, "gotResetTime should be within 1 second of wantResetTime, it was %s seconds off", timeDifference)
+		})
+	}
+}
+
+func createRedisContainer(env e2e.Environment) e2e.Runnable {
+	return env.Runnable("redis").WithPorts(map[string]int{"http": 6379}).Init(
+		e2e.StartOptions{
+			Image:     "redis",
+			Readiness: e2e.NewCmdReadinessProbe(e2e.Command{Cmd: "redis-cli", Args: []string{"ping"}}),
+		},
+	)
+}
